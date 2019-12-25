93c73818d355db13bb539fcefe321d78dad93dec
GaryChen2130 <lt2es.93039@gmail.com>
2019-12-25 11:48:17 +0800

    Merge VGG16 and Image Ranking Engine

---
+++ IRE.py
@@ -0,0 +1,66 @@
+import math
+import numpy as np
+
+class IRE:
+	def __init__(self):
+		self.feature_pool = []
+		self.labels = []
+
+
+	def GetFeature(self,feature_map_list):
+		feature_vector = []
+		for feature_map in feature_map_list:
+			feature_vector.append(self.MaxPooling(feature_map))
+		return feature_vector
+
+
+	def RecordFeature(self,feature_vector,label):
+		self.feature_pool.append(feature_vector)
+		self.labels.append(label)
+		return
+
+
+	def MaxPooling(self,feature_map):
+		return np.max(feature_map.cpu().clone().detach().numpy())
+
+
+	def ComputeSim(self,vector1,vector2):
+		sum_mul = 0
+		sum_square1 = 0
+		sum_square2 = 0
+		for i in range(len(vector1)):
+			sum_mul += vector1[i]*vector2[i]
+			sum_square1 += pow(vector1[i],2)
+			sum_square2 += pow(vector2[i],2)
+		return sum_mul/(math.sqrt(sum_square1)*math.sqrt(sum_square2))
+
+
+	def RankImage(self,image_feature,k):
+		target_vector = self.GetFeature(image_feature)
+
+		sim = []
+		for record_feature in feature_pool:
+			sim.append(self.ComputeSim(target_feature,record_feature))
+
+		rank = []
+		for i in range(len(sim)):
+			rank.append((sim[i],self.labels[i]))
+
+		rank.sort(reverse = True)
+		print(rank)
+
+		return
+
+
+	def Training(self,image_feature,image_label):
+		feature_vector = self.GetFeature(image_feature)
+		self.RecordFeature(feature_vector,image_label)
+		return
+
+
+	def Get_Info(self):
+		features = np.array(self.feature_pool)
+		labels = np.array(self.labels)
+		return features,labels
+		
+
---
+++ VGG_Model.py
@@ -0,0 +1,325 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+from torch.utils.data.sampler import SubsetRandomSampler
+
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+from torchviz import make_dot, make_dot_from_trace
+
+from torchsummary import summary
+
+from PIL import Image
+
+import numpy as np
+import time
+import os
+
+from IRE import *
+
+# Training class
+class Trainer:
+    def __init__(self, criterion, optimizer, device):
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.device = device
+        
+    def train_loop(self, model, train_loader):
+        for epoch in range(EPOCHS):
+            print("---------------- Epoch {} ----------------".format(epoch+1))
+            self._training_step(model, train_loader, epoch)
+            
+            self._validate(model, test_loader, epoch)
+        interface.Save_Info()
+    
+    def test(self, model, test_loader):
+            print("---------------- Testing ----------------")
+            self._validate(model, test_loader, 0, state="Testing")
+            
+    def _training_step(self, model, loader, epoch):
+        model.train()
+        
+        for step, (X, y) in enumerate(loader):
+            X, y = X.to(self.device), y.to(self.device)
+            N = X.shape[0]
+            interface.RecordLabel(y)
+            
+            self.optimizer.zero_grad()
+            outs = model(X)
+            loss = self.criterion(outs, y)
+            
+            if step >= 0 and (step % PRINT_FREQ == 0):
+                self._state_logging(outs, y, loss, step, epoch, "Training")
+            
+            loss.backward()
+            self.optimizer.step()
+        
+        scheduler.step()
+            
+    def _validate(self, model, loader, epoch, state="Validate"):
+        model.eval()
+        outs_list = []
+        loss_list = []
+        y_list = []
+        
+        with torch.no_grad():
+            for step, (X, y) in enumerate(loader):
+                X, y = X.to(self.device), y.to(self.device)
+                N = X.shape[0]
+                
+                outs = model(X)
+                loss = self.criterion(outs, y)
+                
+                y_list.append(y)
+                outs_list.append(outs)
+                loss_list.append(loss)
+            
+            y = torch.cat(y_list)
+            outs = torch.cat(outs_list)
+            loss = torch.mean(torch.stack(loss_list), dim=0)
+            self._state_logging(outs, y, loss, step, epoch, state)
+                
+                
+    def _state_logging(self, outs, y, loss, step, epoch, state):
+        acc = self._accuracy(outs, y)
+        print("[{:3d}/{}] {} Step {:03d} Loss {:.3f} Acc {:.3f}".format(epoch+1, EPOCHS, state, step, loss, acc))
+            
+    def _accuracy(self, output, target):
+        batch_size = target.size(0)
+
+        pred = output.argmax(1)
+        correct = pred.eq(target)
+        acc = correct.float().sum(0) / batch_size
+
+        return acc
+
+# Datasets
+
+# create dataset class
+class Dataset(Dataset):
+    def __init__(self, image_list, label_list, transform=None, train=True):
+        
+        # image_path
+        self.image_path = ''
+        if train == True:
+            self.image_path = TRAIN_PATH
+        else:
+            self.image_path = TEST_PATH
+        
+        # Transforms
+        self.to_tensor = transforms.ToTensor()
+        
+        # image name        
+        self.image_list = image_list
+        
+        # image labels
+        self.label_list = label_list
+        
+        # Calculate len
+        self.data_len = len(self.label_list)
+        
+        # transform
+        self.transform = transform
+                
+    def __len__(self):
+        return self.data_len
+    
+    def __getitem__(self, index):
+        # Get image name from the pandas df
+        single_image_name = self.image_list[index]
+        
+        # Open image and convert to RGB
+        img_as_img = Image.open(self.image_path + single_image_name)
+        img_as_img = img_as_img.convert('RGB')
+        
+        # Transform image to tensor
+        img_as_tensor = self.transform(img_as_img)
+        img_as_tensor = self.to_tensor(img_as_tensor)
+        
+        # Get label(class) of the image based on the cropped pandas column
+        single_image_label = self.label_list[index]
+
+        return (img_as_tensor, single_image_label)
+
+
+# VGG16 Model
+class VGG16(nn.Module):
+    def __init__(self, num_classes=12):
+        super(VGG16, self).__init__()
+        self.features = self._make_layers(cfg['VGG16'])
+        self.feature_map = []
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, num_classes),
+        )
+    
+    def forward(self, x):
+        out = None
+        out = self.features(x)
+        interface.RecordFeature(out)
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        out = self.classifier(out)
+        interface.RecordClass(out)
+        
+        return out
+    
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        return nn.Sequential(*layers)
+
+
+class IRE_Interface:
+    def __init__(self):
+        self.labels = []
+        self.classes = []
+
+    def RecordLabel(self,label_list):
+        self.labels = label_list.cpu().clone().numpy().tolist().copy()
+        return
+
+    def RecordFeature(self,features):
+        for i in range(len(self.labels)):
+            IRE.Training(features[i],self.labels[i])
+        return
+
+    def RecordClass(self,classes):
+        for single_class in classes:
+            self.classes.append(single_class)
+        return
+
+    def Save_Info(self):
+        classes = np.array(self.classes)
+        features,labels = IRE.Get_Info()
+        np.save(PATH_TO_SAVE_DATA + 'classes',classes)
+        np.save(PATH_TO_SAVE_DATA + 'features',features)
+        np.save(PATH_TO_SAVE_DATA + 'labels',labels)
+
+
+if __name__ == '__main__':
+
+    # Hyperparameters
+    cfg = {
+        'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+        'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+        'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+        'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+    }
+
+    labels = {
+        'Abyssinian': 0,
+        'Bengal': 1,
+        'Birman': 2, 
+        'Bombay': 3, 
+        'British_Shorthair': 4,
+        'Egyptian_Mau': 5,
+        'Maine_Coon': 6, 
+        'Persian': 7,
+        'Ragdoll': 8,
+        'Russian_Blue': 9,
+        'Siamese': 10,
+        'Sphynx': 11
+    }
+
+    CIFAR_MEAN = [0.49139968, 0.48215827, 0.44653124]
+    CIFAR_STD = [0.2023, 0.1994, 0.2010]
+
+    EPOCHS = 1
+    BATCH_SIZE = 16
+    PRINT_FREQ = 8
+    TRAIN_NUMS = 49000
+
+    CUDA = True
+
+    PATH_TO_SAVE_DATA = "./"
+
+    TRAIN_PATH = "./dataset_cat/train/"
+    TEST_PATH = "./dataset_cat/test/"
+
+    FILE_PATH = "./dataset_cat/"
+
+    IRE = IRE()
+    interface = IRE_Interface()
+
+    model = VGG16()
+    model.cuda()
+    summary(model, (3, 224, 224))
+
+    # define loss, optimizer and scheduler
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(params=model.parameters(),lr=1e-3, momentum=0.9, weight_decay=1e-3) # weight_decay can be smaller
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=EPOCHS)
+
+    # Get image list and label
+    # Image name
+    name_file = open(TRAIN_PATH + 'train.txt', 'r')
+    train_image_list = name_file.readlines()
+    name_file.close()
+
+    name_file = open(TEST_PATH + 'test.txt', 'r')
+    test_image_list = name_file.readlines()
+    name_file.close()
+
+    # image labels
+    label_file = open(TRAIN_PATH + 'label.txt', 'r')
+    train_label_list = label_file.readlines()
+    label_file.close()
+
+    label_file = open(TEST_PATH + 'label.txt', 'r')
+    test_label_list = label_file.readlines()
+    label_file.close()
+
+    # strip and turn labels to int
+    for i in range(len(train_image_list)):
+        train_image_list[i] = train_image_list[i].strip()
+        train_label_list[i] = int(train_label_list[i].strip())
+
+    for i in range(len(test_image_list)):
+        test_image_list[i] = test_image_list[i].strip()
+        test_label_list[i] = int(test_label_list[i].strip())
+
+    # define datatransform
+    data_transform = transforms.Compose([
+                      transforms.Resize(size=(224, 224))
+                      # data augmentaion
+                    ])
+
+    # make datasets and dataloaders
+    train_dataset = Dataset(train_image_list, train_label_list, data_transform, train=True)
+    test_dataset = Dataset(test_image_list, test_label_list, data_transform, train=False)
+
+    train_loader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=False)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+
+    # CUDA
+    # testing CUDA is available or not
+    if CUDA:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    else:
+        device = torch.device("cpu")
+    print(device)
+
+    # start training
+    trainer = Trainer(criterion, optimizer, device)
+    trainer.train_loop(model, train_loader)
+    trainer.test(model, test_loader)
